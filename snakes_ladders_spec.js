12b1ae6180dddc6e3e73cdae61fca85ec5738cf1
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-28 12:03:35 +0000

    wrote initial test cases

---
+++ snakes_ladders_spec.js
@@ -0,0 +1,56 @@
+// 1. Create board object: has 64 squares, ladder positions (start & end squares), snake positions (start and end squares)
+
+// 2. Create player object: name, counter(colour), score, position, turn_order
+// 3. Create game object: board, list of players, list of counters
+// 4. Initiate game: invite players to join, check if enough players to start (min 4), start game
+// 5. Control turns: check whose turn it is, player rolls die (random 1-6), move player (check if within 6 spaces from end), check on player's new position (is is snake, ladder, final square), check for win
+// 6. End game: give points to winner, end game, start new game?
+
+var Board = require("./snakes_ladders.js").Board;
+var Player = require("./snakes_ladders.js").Player;
+
+var assert =  require("assert");
+
+describe('Board', function(){
+  // 1. Create board object: has 64 squares
+  it("should have 64 squares", function(){
+    var myBoard = new Board();
+    assert.equal(64, myBoard.numberOfSquares);
+  })
+})
+
+describe('Player', function(){
+  // 2. Create player object: name, counter(colour), score, position, turn_order
+  it("should have name, counter(colour), score, position, turn_order", function(){
+    var player1 = new Player("Valerie");
+    assert.equal("Valerie", player1.name);
+  })
+})
+
+describe('Game', function(){
+  // 3. Create game object: board, list of players, list of counters
+  it("should have board", function(){
+    var myBoard = new Board();
+    var myGame = new Game(myBoard, [], ["red", "green", "blue", "yellow"]);
+    assert.equal(myGame.board, myBoard);
+  })
+})
+
+describe('Game', function(){
+  // 3. Create game object: board, list of players
+  it("should have list of players, list of counters", function(){
+    var myBoard = new Board();
+    var myGame = new Game(myBoard, [], ["red", "green", "blue", "yellow"]);
+    assert.equal(myGame.players, []);
+  })
+})
+
+describe('Game', function(){
+  // 3. Create game object: list of counters
+  it("should have list of players, list of counters", function(){
+    var myBoard = new Board();
+    var myGame = new Game(myBoard, [], ["red", "green", "blue", "yellow"]);
+    assert.equal(myGame.counters, ["red", "green", "blue", "yellow"]);
+  })
+})
+
